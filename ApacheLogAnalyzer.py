f5e1dd626ed94de2ab44e43fc3e246cb3c00b9a5
usa6463 <usa6463@naver.com>
2017-05-14 23:24:48 +0900

    modify for python2

--- ApacheLogAnalyzer.py
+++ ApacheLogAnalyzer.py
@@ -14,22 +14,22 @@ if num!=2:
 log_file_name = sys.argv[1]
 num_lines = sum(1 for line in open(log_file_name))
 
-# for saving IP info 
-ip_info_reader = geolite2.reader() 
+# for saving IP info
+ip_info_reader = geolite2.reader()
 ip_info_dict = {}
 
 with open(log_file_name, 'r') as rfd:
 
 	# files open
-	csvfile1 = open('SimpleIPList.csv', 'w', newline='')
+	csvfile1 = open('SimpleIPList.csv', 'w')
 	writer1 = csv.writer(csvfile1)
-	csvfile2 = open('DetailIPList.csv', 'w', newline='')
+	csvfile2 = open('DetailIPList.csv', 'w')
 	writer2 = csv.writer(csvfile2)
-	csvfile4 = open('sqli_list.csv', 'w', newline='')
+	csvfile4 = open('sqli_list.csv', 'w')
 	writer4 = csv.writer(csvfile4)
-	csvfile5 = open('rfi_list.csv', 'w', newline='')
+	csvfile5 = open('rfi_list.csv', 'w')
 	writer5 = csv.writer(csvfile5)
-	csvfile6 = open('webshell_list.csv', 'w', newline='')
+	csvfile6 = open('webshell_list.csv', 'w')
 	writer6 = csv.writer(csvfile6)
 	exceptWriter = open('exception.txt', 'a')
 
@@ -46,11 +46,11 @@ with open(log_file_name, 'r') as rfd:
 	webshell_filter = root.findall('filter')
 
 	# make directory
-	if not 'activity' in os.listdir():
+	if not 'activity' in os.listdir('.'):
 			os.mkdir('activity')
 
 	# For handling case that line has '\n' character
-	line_buffer = '' 
+	line_buffer = ''
 	multiLineCheck = False
 
 	# reading line by line
@@ -66,7 +66,7 @@ with open(log_file_name, 'r') as rfd:
 		# separate fields
 		p = re.compile('(?P<date>\d{4}-\d{2}-\d{2}) (?P<time>\d{2}:\d{2}:\d{2}) (?P<s_ip>\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}) (?P<cs_method>\S+) (?P<cs_uri_stem>\S+) (?P<cs_uri_query>.+?) (?P<s_port>443|80) (?P<cs_username>.+?) (?P<c_ip>\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}) (?P<User_Agent>.+?) (?P<Referer>[-]|[<]a href.*|[(]?http.*|\S+) (?P<sc_status>\d+) (?P<sc_substatus>\d+) (?P<sc_win32_status>\d+) (?P<time_taken>\d+)')
 		m = p.match(line)
-		
+
 		# line has '\n' character case
 		if m and multiLineCheck:
 			exceptWriter.write('{} '.format(i)+line_buffer+'\n')
@@ -94,16 +94,15 @@ with open(log_file_name, 'r') as rfd:
 		activity = list(m.groups())
 		ip = m.group('c_ip')
 		ip_info = ip_info_reader.get(ip)
-		
+
 		# activity write
-		csvfile3 = open('activity/'+ip+'.csv', 'a', newline='')
+		csvfile3 = open('activity/'+ip+'.csv', 'a')
 		writer3 = csv.writer(csvfile3)
 		writer3.writerow(activity)
 		csvfile3.close()
-		
 
 		#add unique ID and ip's country, hits
-		if(not ip in ip_info_dict): 
+		if(not ip in ip_info_dict):
 			country=''
 			if ip_info == None :
 				country = 'NO INFO'
@@ -147,9 +146,9 @@ with open(log_file_name, 'r') as rfd:
 				description = element.findtext("description")
 				writer6.writerow(activity + [description])
 				break
-			
+
 	# list of unique IP addresses with country and number of hits
-	for ip in ip_info_dict: 
+	for ip in ip_info_dict:
 		writer2.writerow([ip]+ip_info_dict[ip])
 
 	csvfile1.close()
@@ -157,5 +156,3 @@ with open(log_file_name, 'r') as rfd:
 	csvfile4.close()
 	csvfile5.close()
 	exceptWriter.close()
-
-    
